51552cdf8290fc49e64af2b3533ae64d73c468a2
Filip <coderaligator@gmail.com>
2022-02-17 06:21:57 +0100

    Fix db connection after reinstall

--- src/main/java/com/kamann/KamannApplication.java
+++ src/main/java/com/kamann/KamannApplication.java
@@ -6,10 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class KamannApplication {
     public static void main(String[] args) {
-
-//        Client client = new Client(1L, "John", "Doe", Date.from(Instant.now()), "password", "124314135", "male", new Address(700900800, "test@test.com", "Pulawska", "53b", "02-508", "Warsaw"));
-//        System.out.println("id: " + client.getId() + " created at: " + client.getCreatedAt() + " info " + client.getAddress());
-    SpringApplication.run(KamannApplication.class, args);
+        SpringApplication.run(KamannApplication.class, args);
     }
-
 }
