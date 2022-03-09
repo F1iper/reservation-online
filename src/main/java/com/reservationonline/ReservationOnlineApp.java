c0dfee151d7a4d53d89a50444558c550610703bf
F1iper <ftimofiejew@gmail.com>
2022-03-09 19:39:44 +0100

    New business app name

--- src/main/java/com/reservationonline/ReservationOnlineApp.java
+++ src/main/java/com/reservationonline/ReservationOnlineApp.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class KamannApplication {
+public class ReservationOnlineApp {
     public static void main(String[] args) {
-        SpringApplication.run(KamannApplication.class, args);
+        SpringApplication.run(ReservationOnlineApp.class, args);
     }
 }
